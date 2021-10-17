0b63e73a19e69a8acf64403c21e5778dca667cf0
ryslig <shupjacob@gmail.com>
2021-10-16 20:11:21 -0400

    Update settings_picture.php

--- pages/settings_picture.php
+++ pages/settings_picture.php
@@ -1,4 +1,36 @@
 <?php
+// functions lifted from https://stackoverflow.com/questions/6891352/crop-image-from-center-php/49851547#49851547
+function cropAlign($image, $cropWidth, $cropHeight, $horizontalAlign = 'center', $verticalAlign = 'middle') {
+    $width = imagesx($image);
+    $height = imagesy($image);
+    $horizontalAlignPixels = calculatePixelsForAlign($width, $cropWidth, $horizontalAlign);
+    $verticalAlignPixels = calculatePixelsForAlign($height, $cropHeight, $verticalAlign);
+    return imageCrop($image, [
+        'x' => $horizontalAlignPixels[0],
+        'y' => $verticalAlignPixels[0],
+        'width' => $horizontalAlignPixels[1],
+        'height' => $verticalAlignPixels[1]
+    ]);
+}
+
+function calculatePixelsForAlign($imageSize, $cropSize, $align) {
+    switch ($align) {
+        case 'left':
+        case 'top':
+            return [0, min($cropSize, $imageSize)];
+        case 'right':
+        case 'bottom':
+            return [max(0, $imageSize - $cropSize), min($cropSize, $imageSize)];
+        case 'center':
+        case 'middle':
+            return [
+                max(0, floor(($imageSize / 2) - ($cropSize / 2))),
+                min($cropSize, $imageSize),
+            ];
+        default: return [0, $imageSize];
+    }
+}
+
 if(isset($_FILES['thumb']['size'])) {
 	if(!empty($_FILES['thumb']['size'])) {
 		if(getimagesize($_FILES['thumb']['tmp_name']) !== false) {
@@ -9,7 +41,12 @@ if(isset($_FILES['thumb']['size'])) {
 					if($mime !== 'image/gif') {
 						if($mime == 'image/jpeg') $image = imagecreatefromjpeg($_FILES['thumb']['tmp_name']);
 						if($mime == 'image/png') $image = imagecreatefrompng($_FILES['thumb']['tmp_name']);
-						$image = imagescale($image, 100);
+						if(imagesx($image) <= imagesy($image)) {
+							$image = imagescale($image, 100, -1, IMG_BICUBIC);
+						} else {
+							$image = imagescale($image, -1, 100, IMG_BICUBIC);
+						}
+						$image = cropAlign($image, 100, 100);
 						imagegif($image, $destination);
 						$_SESSION['alert'] = "Image successfully updated!";
 					} else {
