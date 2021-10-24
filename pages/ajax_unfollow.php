96b8fbdf7aec40d7c806e5618a1a9ff33d588119
ryslig <shupjacob@gmail.com>
2021-10-24 13:06:31 -0400

    Update ajax_unfollow.php

--- pages/ajax_unfollow.php
+++ pages/ajax_unfollow.php
@@ -1,11 +1,6 @@
 <?php
-if(isset($_GET['user'])) {
-	if(isset($_SESSION['username'])) {
-		if(!preg_match("/[^0-9a-zA-Z\s]/", $_GET['user'])) {
-			if(mysqli_num_rows(mysqli_query($conn, "SELECT * FROM `follows` WHERE `follower` = '".$_SESSION['username']."' AND `following` = '".$_GET['user']."';")) == 1) {
-				mysqli_query($conn, "DELETE FROM `follows` WHERE `follower` = '".$_SESSION['username']."' AND `following` = '".$_GET['user']."';");
-			}
-		}
-	}
+if(isset($_GET['user']) && isset($_SESSION['username'])) {
+	$stmt = $conn->prepare("DELETE FROM follows WHERE follower = ? AND following = ?");
+	$stmt->bind_param('ss', $_SESSION['username'], $_GET['user']);
+	$stmt->execute();
 }
-?>
