6cd2f2f17f6541424ce4fc5a25ad219d94cc09fe
ryslig <shupjacob@gmail.com>
2021-10-24 13:12:45 -0400

    Update ajax_delete.php

--- pages/ajax_delete.php
+++ pages/ajax_delete.php
@@ -1,10 +1,6 @@
 <?php
-if(isset($_GET['id'])) {
-	if(isset($_SESSION['username'])) {
-		$row = mysqli_fetch_array(mysqli_query($GLOBALS['conn'], "SELECT * FROM `updates` WHERE `id` = '".intval($_GET['id'])."';"), MYSQLI_ASSOC);
-		if($_SESSION['username'] == $row['author']) {
-			mysqli_query($GLOBALS['conn'], "DELETE FROM `updates` WHERE `id` = '".intval($_GET['id'])."';");
-		}
-	}
+if(isset($_GET['id']) && isset($_SESSION['username'])) {
+	$stmt = $conn->prepare("DELETE FROM updates WHERE id = ? AND author = ?");
+	$stmt->bind_param('is', $_GET['id'], $_SESSION['username']);
+	$stmt->execute();
 }
-?>
